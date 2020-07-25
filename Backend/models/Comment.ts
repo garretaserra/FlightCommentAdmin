d82e96b5852dc996529ed3dcffe8a760d958c70d
garretaserra <garretaserra@gmail.com>
2020-07-25 23:12:20 +0200

    Added some validation at the mongoose level.

--- Backend/models/Comment.ts
+++ Backend/models/Comment.ts
@@ -2,11 +2,16 @@ import mongoose, {Schema} from 'mongoose'
 
 export const CommentSchema: Schema = new Schema<any>({
   // Default index will be named "_id"
-  comment:    {type: String,  required:true},
-  date:       {type: Date,    required: true},
-  UserId:     {type: Number,  required: true},
-  FlightId:   {type: Number,  required: true},
+  comment:    {type: String,  required:true, minlength: 1},             // Avoid empty comments
+  date:       {type: Date,    required: true, validate: checkDates},
+  UserId:     {type: Number,  required: true, min: 0, max:9999999999},  // Limit UserId to 10 digits
+  FlightId:   {type: Number,  required: true, min: 0, max:9999999999},  // Limit FlightId to 10 digits
   Tags:       {type: [String]}
 })
 
 export default mongoose.model('Comment', CommentSchema);
+
+function checkDates(date: Date) {
+  // Check that the date is older than current time and later than Unix epoch
+  return date.getMilliseconds() < Date.now() && date.getMilliseconds() > 0;
+}
