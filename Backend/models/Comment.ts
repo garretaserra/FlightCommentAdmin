43460bddfaf71d5ecc7d7e789aa3321ecf6dd5a0
garretaserra <garretaserra@gmail.com>
2020-07-27 10:17:36 +0200

    Remove validations that are not needed

--- Backend/models/Comment.ts
+++ Backend/models/Comment.ts
@@ -4,8 +4,8 @@ export const CommentSchema: Schema = new Schema<any>({
   // Default index will be named "_id"
   comment:    {type: String,  required:true, minlength: 1},             // Avoid empty comments
   date:       {type: Date,    required: true, validate: checkDates},
-  UserId:     {type: Number,  required: true, min: 0, max:9999999999},  // Limit UserId to 10 digits
-  FlightId:   {type: Number,  required: true, min: 0, max:9999999999},  // Limit FlightId to 10 digits
+  UserId:     {type: Number,  required: true, min: 0},
+  FlightId:   {type: Number,  required: true, min: 0},
   Tags:       {type: [String]}
 })
 
